5ca88e1ccf2705eba359f16a1da0dcacf8e98a94
Amit Garg <amitg@ohumhealthcare.com>
2020-01-27 23:58:30 +0530

    Setup test library

---
+++ Example/TestLibrary/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TestLibrary
+//
+//  Created by Amit Garg on 01/27/2020.
+//  Copyright (c) 2020 Amit Garg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
